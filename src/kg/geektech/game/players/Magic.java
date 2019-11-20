88e4d75e03a3140d9e457665ebf675c24c69b515
IT-kubat < gmail adresskubat88@list.ru>
2019-11-20 21:33:52 +0500

    Логические ошибки

--- src/kg/geektech/game/players/Magic.java
+++ src/kg/geektech/game/players/Magic.java
@@ -3,6 +3,8 @@ package kg.geektech.game.players;
 import kg.geektech.game.Boss.Boss;
 import kg.geektech.game.general.AbilityType;
 
+import java.util.Random;
+
 public class Magic extends Hero {
 
     public Magic(int health, int damage) {
@@ -12,7 +14,9 @@ public class Magic extends Hero {
     @Override
     public void useAbility(Hero[] heroes, Boss boss) {
         for (int i = 0; i < heroes.length-1; i++) {
-            heroes[i].setDamage(heroes[i].getDamage()+2);
+            Random r = new Random();
+            int x = r.nextInt(4)+2;
+            heroes[i].setDamage(heroes[i].getDamage()+x);
             System.out.println("Magic Attack " + heroes[i].getClass().getName() +" damage = "+heroes[i].getDamage());
 
         }
